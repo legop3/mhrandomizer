e950831a06a7cef346501e36ea7d33771635f5d9
legop3 <minecratfd8@gmail.com>
2022-06-16 14:06:02 -0400

    fart

--- randomizer.js
+++ randomizer.js
@@ -1,68 +1,111 @@
 xlsx = require('node-xlsx').default;
 const path = require('path');
 const fs = require('fs');
-const { timeStamp } = require('console');
-const directoryPath = path.join(__dirname, 'spreadsheets');
 
 
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        // console.log(file); 
-    });
+
+const { Client } = require('pg')
+const client = new Client()
+await client.connect()
+
+
+// const { timeStamp } = require('console');
+// const directoryPath = path.join(__dirname, 'spreadsheets');
+var discordarray = []
+var dbarray = []
+
+
+
+
+
+
+
+//gacha armor
+var gachaarmor = xlsx.parse(`./spreadsheets/Gacha_Armor.xlsx`)
+gachaarmor.forEach(element => {
+        console.log('gacha armor')
+            for(var i = 0; i < 10; i++){
+                  console.log(element.data[Math.floor(Math.random()*element.data.length)])
+            }
 });
 
-const getAllFiles = function(dirPath, arrayOfFiles) {
-    files = fs.readdirSync(dirPath)
+
+
+
+
+    // for(var i = 0; i < 9; i++){
+    //     console.log(gachaarmor.data[Math.floor(Math.random()*gachaarmor.data.length)][2]) 
+    // }
+
+ 
+
+
+
+
+
+
+
+
+
+// fs.readdir(directoryPath, function (err, files) {
+//     //handling error
+//     if (err) {
+//         return console.log('Unable to scan directory: ' + err);
+//     } 
+//     //listing all files using forEach
+//     files.forEach(function (file) {
+//         // Do whatever you want to do with the file
+//         // console.log(file); 
+//     });
+// });
+
+// const getAllFiles = function(dirPath, arrayOfFiles) {
+//     files = fs.readdirSync(dirPath)
   
-    arrayOfFiles = arrayOfFiles || []
+//     arrayOfFiles = arrayOfFiles || []
   
-    files.forEach(function(file) {
-      if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-        arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
-      } else {
-        arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
-      }
-    })
+//     files.forEach(function(file) {
+//       if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+//         arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+//       } else {
+//         arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+//       }
+//     })
     
-    return arrayOfFiles
-  }
+//     return arrayOfFiles
+//   }
 
 
 
 
-console.log(getAllFiles("spreadsheets"))
+// console.log(getAllFiles("spreadsheets"))
 
-getAllFiles("spreadsheets").forEach(element => {
-    console.log('wa')
-    // console.log(element)
-    const sheetopen = xlsx.parse(element)
-sheetopen.forEach(element => {
+// getAllFiles("spreadsheets").forEach(element => {
+//     console.log('wa')
+//     // console.log(element)
+//     const sheetopen = xlsx.parse(element)
+// sheetopen.forEach(element => {
+//   console.log(element)
 
+//     // console.log(element)
+//     // element.data.forEach(element => {
+//     //     console.log(element)
+//     //     console.log('waw')
+//     // })
 
-    // console.log(element)
-    // element.data.forEach(element => {
-    //     console.log(element)
-    //     console.log('waw')
-    // })
-
-    for(var i = 0; i < 9; i++){
-        console.log(element.data[Math.floor(Math.random()*element.data.length)][2])
+//     for(var i = 0; i < 9; i++){
+//         console.log(element.data[Math.floor(Math.random()*element.data.length)][2])
         
-    }
+//     }
 
 
 
 
 
 
-})
+// })
 
   
 
-})
+// })
+
