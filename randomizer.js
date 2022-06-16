6f21e8b752aa6ea2f897074cd538d38dd0b6fd91
legop3 <minecratfd8@gmail.com>
2022-06-16 14:06:55 -0400

    f

--- randomizer.js
+++ randomizer.js
@@ -6,8 +6,11 @@ const fs = require('fs');
 
 const { Client } = require('pg')
 const client = new Client()
-await client.connect()
-
+client.connect()
+client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message) // Hello World!
+  client.end()
+})
 
 // const { timeStamp } = require('console');
 // const directoryPath = path.join(__dirname, 'spreadsheets');
