0828826a8c000562f131c86d5f1c808549707970
lucky0wl-sketch <Brodywelch9@gmail.com>
2022-09-16 17:10:38 -0400

    1

--- randomizer.js
+++ randomizer.js
@@ -184,7 +184,7 @@ function addtodiscord9(japname, engname) {
 
  
 function addtodb(itemid) {
-  dbarray.push([10, 8, counter++, itemid, 1000, 1, 0, 0, 1, 1, 0, 1, 40, 0, null])
+  dbarray.push([10, 8, counter++, itemid, 1000, 1, 0, 0, 1, 1, 0, 1, 40, 0])
 }
 
   dbarray.push([10, 8, 1, 5755, 25, 50, 0, 0, 1, 1, 0, 1, 5, 0, null])
